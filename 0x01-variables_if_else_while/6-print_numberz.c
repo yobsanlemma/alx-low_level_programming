b7eac3a018d6261329846f663c785dad2e1f6418
yobsan lemma <yobsanlemma13@gmail.com>
2022-10-10 07:44:40 +0300

    commit projects

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,10 +2,12 @@
 
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
- * followed by a new line, using putchar
+ * followed by a new line using putchar.
+ *
  * Return: Always 0 (Success)
  */
-int main(void)
+
+int main (void)
 {
 	int n;
 
